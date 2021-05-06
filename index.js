073a1cab6d7a093be96d9de19ea23817cec823e2
Oliver Brandon Bravo Cedillo <oliver.bravoc@gmail.com>
2021-05-06 14:12:18 -0500

    fix: delete credentials

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// HOSTNAME=database-testing-1.codai4geyrzp.us-east-2.rds.amazonaws.com USER=admintesting PASSWORD=tG5ThCAEk7T0 DB=Testing1 node index.js
+
 const app = require('./app');
 
 app.listen(3002,() => {
